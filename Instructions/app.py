321e9da45e825eacf3ba09c2f75ae940adfe33d3
Bernardo <bernabecia28@gmail.com>
2020-11-17 17:55:12 -0600

    SQLAlchemy commit

---
+++ Instructions/app.py
@@ -0,0 +1,109 @@
+# Dependencies and Setup
+import numpy as np
+import datetime as dt
+from flask import Flask, jsonify
+
+# Python SQL Toolkit and ORM
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.pool import StaticPool
+
+# Create engine
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Reflect Database Into a New Model
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+
+# Save Table References 
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create Session 
+session = Session(engine)
+
+# Setup Flask
+app = Flask(__name__)
+
+# Flask Routes
+# Home Route
+@app.route("/")
+def welcome():
+    # List all routes that are available
+    return (
+        f"Available Route:<br/>"
+        f"/api.v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+# Precipitation Route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+        # Convert the Query Results to a Dictionary Using `date` as the Key and `prcp` as the Value
+        # Calculate the Date 1 Year Ago from the Last Data Point in the Database
+        one_year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
+        # Design a Query to Retrieve the Last 12 Months of Precipitation Data Selecting Only the `date` and `prcp` Values
+        prcp_data = session.query(Measurement.date, Measurement.prcp).\
+                filter(Measurement.date >= one_year_ago).\
+                order_by(Measurement.date).all()
+        # Convert List of Tuples Into a Dictionary
+        prcp_data_list = dict(prcp_data)
+        # Return JSON Representation of Dictionary
+        return jsonify(prcp_data_list)
+
+# Station Route
+@app.route("/api/v1.0/stations")
+def stations():
+        # Return a JSON List of Stations From the Dataset
+        stations_all = session.query(Station.station, Station.name).all()
+        # Convert List of Tuples Into Normal List
+        station_list = list(stations_all)
+        # Return JSON List of Stations from the Dataset
+        return jsonify(station_list)
+
+# TOBs (Total Observations) Route
+@app.route("/api/v1.0/tobs")
+def tobs():
+        # Query for the Dates and Temperature Observations from a Year from the Last Data Point
+        one_year_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
+        # Design a Query to Retrieve the Last 12 Months of Precipitation Data Selecting Only the `date` and `prcp` Values
+        tobs_data = session.query(Measurement.date, Measurement.tobs).\
+                filter(Measurement.date >= one_year_ago).\
+                order_by(Measurement.date).all()
+        # Convert List of Tuples Into Normal List
+        tobs_data_list = list(tobs_data)
+        # Return JSON List of Temperature Observations (tobs) for the Previous Year
+        return jsonify(tobs_data_list)
+
+# Start Day Route
+@app.route("/api/v1.0/<start>")
+def start_day(start):
+        start_day = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start).\
+                group_by(Measurement.date).all()
+        # Convert List of Tuples Into Normal List
+        start_day_list = list(start_day)
+        # Return JSON List of Min Temp, Avg Temp and Max Temp for a Given Start Range
+        return jsonify(start_day_list)
+
+# Start-End Day Route
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_day(start, end):
+        start_end_day = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                filter(Measurement.date >= start).\
+                filter(Measurement.date <= end).\
+                group_by(Measurement.date).all()
+        # Convert List of Tuples Into Normal List
+        start_end_day_list = list(start_end_day)
+        # Return JSON List of Min Temp, Avg Temp and Max Temp for a Given Start-End Range
+        return jsonify(start_end_day_list)
+
+# Define Main Behavior
+if __name__ == '__main__':
+    app.run(debug=True)
